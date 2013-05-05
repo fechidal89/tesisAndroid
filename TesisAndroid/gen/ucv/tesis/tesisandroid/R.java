6f04c64a1e680679e362348fd5e934c3f826cdcd
Fernando Hidalgo <fechidal89@gmail.com>
2013-05-04 23:56:52 -0430

    Se agregó el trabajo realizado en el seminario (TAB, @Strings en Value, permisos en el AndroidManifest, ID en el R.java, etc)

--- TesisAndroid/gen/ucv/tesis/tesisandroid/R.java
+++ TesisAndroid/gen/ucv/tesis/tesisandroid/R.java
@@ -24,7 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000f;
+        public static final int button1=0x7f08000c;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080005;
+        public static final int radio0=0x7f080008;
+        public static final int radio1=0x7f080009;
+        public static final int radioGroup1=0x7f080007;
+        public static final int tab1=0x7f080001;
+        public static final int tab2=0x7f08000b;
+        public static final int tab3=0x7f08000d;
+        public static final int tabhost=0x7f080000;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f08000e;
+        public static final int toggleButton1=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_agent_droid=0x7f030000;
@@ -33,9 +48,28 @@ public final class R {
         public static final int agent_droid=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        /**  TAB 2 - Service Send Traps  
+         */
+        public static final int bTraps=0x7f05000e;
+        public static final int button_OFF=0x7f05000d;
+        public static final int button_ON=0x7f05000c;
+        /**  TAB 1 - Config Agent  
+         */
+        public static final int community=0x7f050005;
+        public static final int communityDefault=0x7f050006;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int port=0x7f050007;
+        public static final int portDefault=0x7f050008;
+        public static final int snmp1=0x7f05000a;
+        public static final int snmp2=0x7f05000b;
+        /**  TAB 3 - System Group  
+         */
+        public static final int systemGroup=0x7f05000f;
+        public static final int title_activity_main=0x7f050003;
+        public static final int version=0x7f050009;
     }
     public static final class style {
         /** 
--- TesisAndroid/src/ucv/tesis/tesisandroid/AgentDroid.java
+++ TesisAndroid/src/ucv/tesis/tesisandroid/AgentDroid.java
@@ -1,15 +1,76 @@
 package ucv.tesis.tesisandroid;
 
+import ucv.tesis.tesisandroid.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.content.Context;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
 import android.view.Menu;
 
 public class AgentDroid extends Activity {
 
+	TabHost tabHost;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agent_droid);
+		 tabHost = (TabHost) findViewById(R.id.tabhost);
+	        System.out.println(tabHost.getId());
+	        // invocamos el setup de TabHost
+	        tabHost.setup();
+	        // Creamos un TabSpec por cada tab
+	        TabSpec specs = tabHost.newTabSpec("tag1");
+	        specs.setContent(R.id.tab1);
+	        specs.setIndicator("Config Agent");
+	        tabHost.addTab(specs);
+	        
+	        specs = tabHost.newTabSpec("tag2");
+	        specs.setContent(R.id.tab2);
+	        specs.setIndicator("Sender Traps");
+	        tabHost.addTab(specs);
+	        
+	        specs = tabHost.newTabSpec("tag3");
+	        specs.setContent(R.id.tab3);
+	        specs.setIndicator("System Group");
+	        tabHost.addTab(specs);
+
+	        Toast toast1 = Toast.makeText(getApplicationContext(),
+	                "No Connected", Toast.LENGTH_SHORT);
+	        
+	        
+	        try {
+	        	ConnectivityManager connec = null;
+	            connec =  (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+	            
+	            if (connec != null) {
+
+	                NetworkInfo net = connec.getActiveNetworkInfo();
+	                
+	                if (net != null && net.getType() == ConnectivityManager.TYPE_WIFI) 
+	                	toast1 = Toast.makeText(getApplicationContext(),
+	                            "AgentDroid by WIFI", Toast.LENGTH_SHORT);			
+	                else
+	        			toast1 = Toast.makeText(getApplicationContext(),
+	                            "AgentDroid connected by other network", Toast.LENGTH_SHORT);
+	        			
+	    		}
+	            
+			} catch (Exception e) {
+				// TODO: handle exception
+				for (int i = 0; i < 100; i++) {
+
+					System.out.println("Error en apertura de conexion!!!");
+						
+				}
+			}
+	        
+	        toast1.show();
 	}
 
 	@Override
