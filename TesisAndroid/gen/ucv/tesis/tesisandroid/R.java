bf9482d4fdd6372b335031ec74ce150cfefe3841
Fernando Hidalgo <fechidal89@gmail.com>
2013-05-19 23:33:44 -0430

    Clase R con todas las view agregadas y generadas automaticamente

--- TesisAndroid/gen/ucv/tesis/tesisandroid/R.java
+++ TesisAndroid/gen/ucv/tesis/tesisandroid/R.java
@@ -8,6 +8,9 @@
 package ucv.tesis.tesisandroid;
 
 public final class R {
+    public static final class array {
+        public static final int typetraps=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,59 +20,115 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int button1=0x7f08000c;
-        public static final int editText1=0x7f080003;
-        public static final int editText2=0x7f080005;
-        public static final int radio0=0x7f080008;
-        public static final int radio1=0x7f080009;
-        public static final int radioGroup1=0x7f080007;
-        public static final int tab1=0x7f080001;
-        public static final int tab2=0x7f08000b;
-        public static final int tab3=0x7f08000d;
-        public static final int tabhost=0x7f080000;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080006;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f08000e;
-        public static final int toggleButton1=0x7f08000a;
+        public static final int action_settings=0x7f09002b;
+        public static final int editText1=0x7f090003;
+        public static final int editText2=0x7f090005;
+        public static final int layoutForScroll=0x7f09000b;
+        public static final int radio0=0x7f090008;
+        public static final int radio1=0x7f090009;
+        public static final int radioGroup1=0x7f090007;
+        public static final int switchService=0x7f09002a;
+        public static final int tab1=0x7f090001;
+        public static final int tab2=0x7f09000a;
+        public static final int tab2Spinner1=0x7f090013;
+        public static final int tab2TextView1=0x7f09000c;
+        public static final int tab2TextView2=0x7f09000e;
+        public static final int tab2button1=0x7f09001c;
+        public static final int tab2editText1=0x7f09000d;
+        public static final int tab2editText2=0x7f09000f;
+        public static final int tab2editText3=0x7f090011;
+        public static final int tab2editText4=0x7f090017;
+        public static final int tab2editText5=0x7f090015;
+        public static final int tab2editText6=0x7f090019;
+        public static final int tab2editText7=0x7f09001b;
+        public static final int tab2textView3=0x7f090010;
+        public static final int tab2textView4=0x7f090012;
+        public static final int tab2textView5=0x7f090016;
+        public static final int tab2textView6=0x7f090014;
+        public static final int tab2textView7=0x7f090018;
+        public static final int tab2textView8=0x7f09001a;
+        public static final int tab3=0x7f09001d;
+        public static final int tab3editText1=0x7f090023;
+        public static final int tab3editText2=0x7f090025;
+        public static final int tab3editText3=0x7f090027;
+        public static final int tab3textView0=0x7f09001e;
+        public static final int tab3textView1=0x7f09001f;
+        public static final int tab3textView2=0x7f090020;
+        public static final int tab3textView3=0x7f090021;
+        public static final int tab3textView4=0x7f090022;
+        public static final int tab3textView5=0x7f090024;
+        public static final int tab3textView6=0x7f090026;
+        public static final int tab3textView7=0x7f090028;
+        public static final int tabhost=0x7f090000;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090006;
+        public static final int textView3=0x7f090004;
+        public static final int toggleButton1=0x7f090029;
     }
     public static final class layout {
         public static final int activity_agent_droid=0x7f030000;
+        public static final int compound_button=0x7f030001;
     }
     public static final class menu {
-        public static final int agent_droid=0x7f070000;
+        public static final int agent_droid=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        /**  TAB 2 - Service Send Traps  
-         */
-        public static final int bTraps=0x7f05000e;
-        public static final int button_OFF=0x7f05000d;
-        public static final int button_ON=0x7f05000c;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int bTraps=0x7f060018;
         /**  TAB 1 - Config Agent  
          */
-        public static final int community=0x7f050005;
-        public static final int communityDefault=0x7f050006;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int port=0x7f050007;
-        public static final int portDefault=0x7f050008;
-        public static final int snmp1=0x7f05000a;
-        public static final int snmp2=0x7f05000b;
+        public static final int community=0x7f060005;
+        public static final int communityDefault=0x7f060006;
+        /**  TAB 2 - Service Send Traps  
+         */
+        public static final int dtsIpAddress=0x7f060014;
+        public static final int enterpriseOID=0x7f06000f;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int port=0x7f060007;
+        public static final int portDefault=0x7f060008;
+        /**  
+    <string name="port">Port:</string> 
+    
+         */
+        public static final int portDefaultTraps=0x7f060015;
+        public static final int retries=0x7f060010;
+        public static final int retriesDefault=0x7f060011;
+        public static final int snmp1=0x7f06000a;
+        public static final int snmp2=0x7f06000b;
+        public static final int snmpServiceOff=0x7f06000e;
+        public static final int snmpServiceOn=0x7f06000d;
+        public static final int specificTrap=0x7f060017;
+        public static final int switchService=0x7f06000c;
+        public static final int sysContact=0x7f06001e;
+        public static final int sysDescr=0x7f06001b;
+        public static final int sysLocation=0x7f060020;
+        public static final int sysName=0x7f06001f;
+        public static final int sysObjectID=0x7f06001c;
+        public static final int sysServices=0x7f060021;
+        public static final int sysUpTime=0x7f06001d;
+        public static final int systemGroup=0x7f06001a;
+        public static final int timeOut=0x7f060012;
+        public static final int timeOutDefault=0x7f060013;
+        public static final int title_activity_main=0x7f060003;
+        /**  
+    <string name="community">Community:</string>
+    <string name="communityDefault">Public</string>
+     
+         */
+        public static final int trapType=0x7f060016;
         /**  TAB 3 - System Group  
          */
-        public static final int systemGroup=0x7f05000f;
-        public static final int title_activity_main=0x7f050003;
-        public static final int version=0x7f050009;
+        public static final int valueNull=0x7f060019;
+        public static final int version=0x7f060009;
     }
     public static final class style {
         /** 
@@ -93,10 +152,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
