b18ee90d73fd07efa3d7ec56ef5a132e36213cae
Fernando Hidalgo <fechidal89@gmail.com>
2013-07-14 01:36:52 -0430

    Clase que hereda se Service para implementar el servicio de SNMP

---
+++ TesisAndroid/src/ucv/tesis/tesisandroid/AgentSNMP.java
@@ -0,0 +1,57 @@
+package ucv.tesis.tesisandroid;
+
+import snmp.SNMPv1AgentInterface;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class AgentSNMP extends Service {
+	
+	SNMPv1AgentInterface agent = null;
+	private final IBinder myBinder = new LocalBinder();
+	
+	
+	public AgentSNMP() {}
+	
+	@Override
+	public void onCreate(){
+		try {
+			this.agent = new SNMPv1AgentInterface(1);
+			this.agent.startReceiving();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public void onDestroy(){
+		super.onDestroy();
+		
+		try {
+			if (agent != null){ 
+				agent.stopReceiving(); 
+			}
+			Toast.makeText(this, "Servicio Detenido...!", Toast.LENGTH_SHORT).show();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		return this.myBinder;
+	}
+	
+	
+	public class LocalBinder extends Binder {
+		AgentSNMP getService() {
+            return AgentSNMP.this;
+		}
+	}
+}
