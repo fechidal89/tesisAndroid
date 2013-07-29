3824f27d7de5752a428ea74e218967337be28d4c
Fernando Hidalgo <fechidal89@gmail.com>
2013-07-29 01:06:29 -0430

    Se programa la el OID IfNumber y la tabla Iftable, tabla de interfaces de red con los valores de ifEntry. <Codigo en Prueba>

--- TesisAndroid/src/ucv/tesis/tesisandroid/Mibs.java
+++ TesisAndroid/src/ucv/tesis/tesisandroid/Mibs.java
@@ -1,9 +1,17 @@
 package ucv.tesis.tesisandroid;
 
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+
 import snmp.SNMPBERCodec;
 import snmp.SNMPBadValueException;
 import snmp.SNMPGetException;
+import snmp.SNMPInteger;
 import snmp.SNMPObject;
 import snmp.SNMPObjectIdentifier;
 import snmp.SNMPOctetString;
@@ -163,9 +171,98 @@ public class Mibs implements SNMPRequestListener {
                     }
                 } 
                 
+            } // if 
+            
+            
+            
+            /******************************
+             *
+             *   MIB-II | the Interfaces Group
+             * 
+             ******************************/
+        
+            /* ifNumber Integer | Read-Only | Mandatory */
+            if (snmpOID.toString().equals("1.3.6.1.2.1.2.1.0"))
+            {
+                if (pduType == SNMPBERCodec.SNMPSETREQUEST)
+                {
+                    int errorIndex = i+1;
+                    int errorStatus = SNMPRequestException.VALUE_READ_ONLY;
+                    throw new SNMPSetException("Trying to set a read-only variable!", errorIndex, errorStatus);
+                }
+                else if (pduType == SNMPBERCodec.SNMPGETREQUEST)
+                {
+                    int nIf = 0; // numero de interfaces
+                	ArrayList<String> arrIf = new ArrayList<String>();
+                	String cmd="";
+                	try
+                    {
+                        Process p=null;
+                        try {
+							p = Runtime.getRuntime().exec("ls /sys/class/net/");
+							BufferedReader in2 = new BufferedReader(new InputStreamReader(p.getInputStream()));
+							String line;
+					        while ((line = in2.readLine()) != null) {  
+					        	nIf += 1; 
+					        	arrIf.add(line);
+					        }
+
+					        /** Crear tabla de if  **/
+					        
+					        String [] arrEntry = {
+					        		"type", /* ifType */
+					        		"mtu", /* ifMtu */
+					        		"speed", /* IfSpeed */
+					        		"address", /* ifPhyAddress */
+					        		"operstate", /* ifOperStatus */  
+					        		/**  ###FALTA### IfAdminStatus **/
+					        		"power/runtime_active_time", /* ifLastChange */
+					        		"statistics/rx_bytes", /*ifInOctets*/
+					        		"statistics/rx_packets", /*IfInUcastPkts*/
+					        		"statistics/multicast", /* ifInNUcastPkts*/
+					        		"statistics/rx_dropped", /* ifInDiscards*/
+					        		"statistics/rx_errors", /* ifInErrors*/
+					        		"statistics/rx_missed", /** ifInUnknownProtos  PREGUNTAR */
+					        		"statistics/tx_bytes", /* ifOutOctets */
+					        		"statistics/tx_packets", /* ifOutUcastPkts */
+					        		/** ###FALTA### ifOutNUcastPkts */
+					        		"statistics/tx_dropped", /* ifOutDiscards */
+					        		"statistics/tx_errors", /* ifOutErrors */
+					        		"tx_queue_len", /* ifOutErrors */
+						        	};
+					        
+					        for (String entry : arrEntry) {
+								System.out.println("TABLE ENTRY => "+entry);
+						        for (String eth : arrIf) {
+						        	cmd="cat /sys/class/net/"+eth+"/"+entry+"\n";
+						        	//System.out.println(cmd);
+						        	p = Runtime.getRuntime().exec(cmd);
+									in2 = new BufferedReader(new InputStreamReader(p.getInputStream()));
+							        System.out.println("if: \""+eth+"\" => "+in2.readLine()); 
+						            cmd="";
+						            
+							    }
+					        }
+					        
+					        p.destroy();
+					        
+					    } catch (Exception e) {
+							// TODO: handle exception
+						}
+                        
+                        SNMPVariablePair newPair = new SNMPVariablePair(new SNMPObjectIdentifier(snmpOID.toString()), new SNMPInteger(nIf));
+                        responseList.addSNMPObject(newPair);
+                        
+                    }
+                    catch (SNMPBadValueException e)
+                    {
+                        // won't happen...
+                    }
+                } 
+                    
             }
             
-        }
+        } // for
         
         System.out.println("\n");
         
