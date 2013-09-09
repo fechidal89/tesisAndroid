83b9ec5f61ae13cf376a0746dc695c27b663d327
Fernando Hidalgo <fechidal89@gmail.com>
2013-09-08 23:46:03 -0430

    Clase para recorrer el árbol para los paquetes GetNextRequest.

---
+++ TesisAndroid/src/ucv/tesis/tesisandroid/SNMPTreeGetNextRequestIter.java
@@ -0,0 +1,71 @@
+package ucv.tesis.tesisandroid;
+import java.util.Iterator;
+
+public class SNMPTreeGetNextRequestIter<T> implements Iterator<SNMPTreeGetNextRequest<T>>{
+
+	        enum ProcessStages {
+	                ProcessParent, ProcessChildCurNode, ProcessChildSubNode
+	        }
+
+	        private SNMPTreeGetNextRequest<T> treeNode;
+
+	        public SNMPTreeGetNextRequestIter(SNMPTreeGetNextRequest<T> treeNode) {
+	                this.treeNode = treeNode;
+	                this.doNext = ProcessStages.ProcessParent;
+	                this.childrenCurNodeIter = treeNode.children.iterator();
+	        }
+
+	        private ProcessStages doNext;
+	        private SNMPTreeGetNextRequest<T> next;
+	        private Iterator<SNMPTreeGetNextRequest<T>> childrenCurNodeIter;
+	        private Iterator<SNMPTreeGetNextRequest<T>> childrenSubNodeIter;
+
+	        @Override
+	        public boolean hasNext() {
+
+	                if (this.doNext == ProcessStages.ProcessParent) {
+	                        this.next = this.treeNode;
+	                        this.doNext = ProcessStages.ProcessChildCurNode;
+	                        return true;
+	                }
+
+	                if (this.doNext == ProcessStages.ProcessChildCurNode) {
+	                        if (childrenCurNodeIter.hasNext()) {
+	                        		SNMPTreeGetNextRequest<T> childDirect = childrenCurNodeIter.next();
+	                                childrenSubNodeIter = childDirect.iterator();
+	                                this.doNext = ProcessStages.ProcessChildSubNode;
+	                                return hasNext();
+	                        }
+
+	                        else {
+	                                this.doNext = null;
+	                                return false;
+	                        }
+	                }
+	                
+	                if (this.doNext == ProcessStages.ProcessChildSubNode) {
+	                        if (childrenSubNodeIter.hasNext()) {
+	                                this.next = childrenSubNodeIter.next();
+	                                return true;
+	                        }
+	                        else {
+	                                this.next = null;
+	                                this.doNext = ProcessStages.ProcessChildCurNode;
+	                                return hasNext();
+	                        }
+	                }
+
+	                return false;
+	        }
+
+	        @Override
+	        public SNMPTreeGetNextRequest<T> next() {
+	                return this.next;
+	        }
+
+	        @Override
+	        public void remove() {
+	                throw new UnsupportedOperationException();
+	        }
+	        
+}
